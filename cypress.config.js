2f0d0fe3c63e35350a77b3964e8b515e134666ca
QAZebass <ze.basszender@gmail.com>
2023-05-01 20:49:28 -0300

    Timeout added and test EC-008 fix

--- cypress.config.js
+++ cypress.config.js
@@ -2,19 +2,19 @@
 import { defineConfig } from 'cypress';
 
 export default defineConfig({
-	projectId: "a8ja1w",
+	projectId: 'a8ja1w',
 	viewportWidth: 1360,
 	viewportHeight: 768,
-	reporter: "cypress-multi-reporters",
-	
-	reporterOptions:{
-		configFile: 'jsconfig.json'
+	reporter: 'cypress-multi-reporters',
+	retries: 1,
+	reporterOptions: {
+		configFile: 'jsconfig.json',
 	},
 	e2e: {
 		setupNodeEvents() {
 			// implement node event listeners here
 		},
-		specPattern: ['**/*.feature','cypress/e2e/**/*.cy.{js,jsx,ts,tsx}'],
+		specPattern: ['**/*.feature', 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}'],
 		baseUrl: 'https://qa-challenge.ensolvers.com',
 		WatchForFileChanges: false,
 	},
