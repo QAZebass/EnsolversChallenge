d06e5e7aea568e548e81db6ed7c694a6fe4b6e02
QAZebass <ze.basszender@gmail.com>
2023-05-01 20:36:54 -0300

    Remove log added

--- cypress/e2e/Ensolvers/EnsolversChallenge.cy.js
+++ cypress/e2e/Ensolvers/EnsolversChallenge.cy.js
@@ -584,3 +584,18 @@ describe('Ensolvers Challenge', () => {
 			});
 	});
 });
+
+// ** Importing this function prevents the UNCAUGHT EXCEPTION AND FETCH from happening.
+Cypress.on('uncaught:exception', () => {
+	// returning false here prevents Cypress from
+	// failing the test
+	return false;
+});
+// Comando predeterminado para que no aparezcan los Fetch en el log del Test Runner:
+const origLog = Cypress.log;
+Cypress.log = function (opts, ...other) {
+	if (opts.displayName === 'xhr' || (opts.displayName === 'fetch' && opts.url.startsWith('https://'))) {
+		return;
+	}
+	return origLog(opts, ...other);
+};
